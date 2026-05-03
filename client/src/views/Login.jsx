ad1ee91d43f208cc5a3b43c5038b90a93035e3d8
avigunjal07 <avishkargunjal07@gmail.com>
2026-05-03 21:50:15 +0530

    login and signup pages created

---
+++ client/src/views/Login.jsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+import { setPageTitle } from "../utils.jsx";
+import Button from "../components/Button.jsx";
+import Input from "../components/Input.jsx";
+import axios from "axios";
+import Navbar from "../components/Navbar.jsx";
+
+import toast, { Toaster } from "react-hot-toast";
+import { Link } from "react-router";
+
+function Login() {
+  useEffect(() => {
+    setPageTitle("Login - TinyTours");
+  }, []);
+
+  const [loginUser, setLoginUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const checkUserLogin = async () => {
+    const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/login` , loginUser);
+
+    console.log(response.data);
+
+    if (response.data.success) {
+      toast.success(response.data.message, { id: "loginSuccess" });
+      setLoginUser({
+        email: "",
+        password: "",
+      });
+
+      const { jwtToken, data } = response.data;
+
+      // localStorage.setItem("token", jwtToken);
+      localStorage.setItem("userJwtToken", jwtToken);
+      // localStorage.setItem("userJwtToken", response.data.token);
+      localStorage.setItem("userData", JSON.stringify(data));
+      
+      setTimeout(() => {
+        window.location.href = "/";
+      }, 500);
+    
+    } else {
+      toast.error(response.data.message, { id: "loginError" });
+    }
+  };
+
+  return (
+    <div>
+        <Navbar />
+     
+
+      <div className="w-75 block mx-auto mt-10">
+        <Input
+          type="email"
+          placeholder="Email"
+          value={loginUser.email}
+          onChange={(e) =>
+            setLoginUser({ ...loginUser, email: e.target.value })
+          }
+        />
+
+        <Input
+          type="password"
+          placeholder="Password"
+          value={loginUser.password}
+          onChange={(e) =>
+            setLoginUser({ ...loginUser, password: e.target.value })
+          }
+        />
+
+        <Button title="Login" onClick={checkUserLogin} />
+        <Link to="/signup" className="my-2 block text-blue-500">
+          Don't have an account? Signup
+        </Link>
+      </div>
+      <Toaster />
+    </div>
+  );
+}
+
+export default Login;
---
+++ client/src/views/Signup.jsx
@@ -0,0 +1,115 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { setPageTitle } from "../utils.jsx";
+import Input from "../components/Input.jsx";
+import Button from "../components/Button.jsx";
+import toast, { Toaster } from "react-hot-toast";
+import { Link } from "react-router";
+import Navbar from "../components/Navbar.jsx";
+
+
+function Signup() {
+  const [newUser, setNewUser] = useState({
+    name: "",
+    email: "",
+    mobile: "",
+    city: "",
+    country: "",
+    password: "",
+  });
+
+  useEffect(() => {
+    setPageTitle("Signup - TinyTours");
+  }, []);
+
+  const createUser = async () => {
+    try {
+      const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/signup`,
+        newUser,
+      );
+
+      if (response.data.success) {
+        toast.success(response.data.message, { id: "signupSuccess" });
+        setNewUser({
+          name: "",
+          email: "",
+          mobile: "",
+          city: "",
+          country: "",
+          password: "",
+        });
+
+        setTimeout(() => {
+          window.location.href = "/login";
+        }, 1000);
+      } else {
+        toast.error(response.data.message, { id: "signupError" });
+      }
+    } catch (error) {
+      if (error.response) {
+        toast.error(error.response.data.message);
+      } else {
+        toast.error("Server error");
+      }
+    }
+  };
+  return (
+    <div>
+      <Navbar />
+      <h1>Signup</h1>
+
+      <div className="w-75 block mx-auto ">
+        <Input
+          type="text"
+          placeholder="Name"
+          value={newUser.name}
+          onChange={(e) => setNewUser({ ...newUser, name: e.target.value })}
+        />
+
+        <Input
+          type="email"
+          placeholder="Email"
+          value={newUser.email}
+          onChange={(e) => setNewUser({ ...newUser, email: e.target.value })}
+        />
+
+        <Input
+          type="text"
+          placeholder="Mobile"
+          value={newUser.mobile}
+          onChange={(e) => setNewUser({ ...newUser, mobile: e.target.value })}
+        />
+
+        <Input
+          type="text"
+          placeholder="Country"
+          value={newUser.country}
+          onChange={(e) => setNewUser({ ...newUser, country: e.target.value })}
+        />
+
+        <Input
+          type="text"
+          placeholder="City"
+          value={newUser.city}
+          onChange={(e) => setNewUser({ ...newUser, city: e.target.value })}
+        />
+
+        <Input
+          type="password"
+          placeholder="Password"
+          value={newUser.password}
+          onChange={(e) => setNewUser({ ...newUser, password: e.target.value })}
+        />
+
+        <Button title="Signup" onClick={createUser} />
+
+        <Link to="/login" className="my-2 block text-blue-500">
+          Already have an account? Login
+        </Link>
+      </div>
+      <Toaster />
+    </div>
+  );
+}
+
+export default Signup;
