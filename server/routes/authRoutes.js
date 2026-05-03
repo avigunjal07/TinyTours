413eee51369f43a531fdfea2d14f5520219c4625
avigunjal07 <avishkargunjal07@gmail.com>
2026-05-03 19:58:43 +0530

    api testing is done for login and signup

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,4 +6,18 @@ const router = express.Router();
 router.post("/signup", postSignup);
 router.post("/login", postLogin);
 
+router.get("/", (req, res) => {
+  res.send("Auth API working");
+});
+
+import { checkJWT } from "../middlewares/jwt.js";
+
+router.get("/profile", checkJWT, (req, res) => {
+  res.json({
+    success: true,
+    message: "Protected route accessed",
+    user: req.user,
+  });
+});
+
 export default router;
